3342e7e5004c209966d18149a97127eb176464e9
WalterCorsini <corsini.walter@gmail.com>
2024-06-18 22:54:49 +0200

    add logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="d-flex justify-content-between align-items-center ps-5 pe-5">
-    <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="logo">
+    <img src="{{ Vite::asset('resources/img/logoComics.png') }}" alt="logo">
     <ul class="d-flex justify-content-center align-items-center gap-3">
         <li class="{{ Request::route()->getName() === 'dccomics.index' ? 'active' : '' }}"><a href="{{ route('dccomics.index') }}">Home</a></li>
         <li class="{{ Request::route()->getName() === 'dccomics.create' ? 'active' : '' }}"><a href="{{ route('dccomics.create') }}">Aggiungi</a></li>
