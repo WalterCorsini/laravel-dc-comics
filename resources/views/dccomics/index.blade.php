42553b18d24653c93d5da7a3813d1739eaf1d6cf
WalterCorsini <corsini.walter@gmail.com>
2024-06-18 19:56:28 +0200

    add comment in index page

--- resources/views/dccomics/index.blade.php
+++ resources/views/dccomics/index.blade.php
@@ -1,13 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    @foreach ($dcComicsList as $curComic)
+    {{-- container --}}
+    <div class="container">
 
+        {{-- card- container --}}
+        @foreach ($dcComicsList as $curComic)
             <div class="card-container">
+
                 {{-- title --}}
                 <div class="card-title">
-                    <span><strong> Titolo: </strong></span>
+                    <span>
+                        <strong>Titolo: </strong>
+                    </span>
                     <p>
                         {{ $curComic->title }}
                     </p>
@@ -22,28 +27,43 @@
 
                 {{-- section btn --}}
                 <div class="card-button">
+                    {{-- btn add record --}}
                     <a class="btn btn-info text-white text-decoration-none"
-                        href="{{ route('dccomics.show', ['dccomic' => $curComic->id]) }}"><i class="fa-solid fa-plus"></i></a>
-                    <a class="btn btn-success text-decoration-none"
-                        href="{{ route('dccomics.edit', ['dccomic' => $curComic->id]) }}"><i class="fa-solid fa-pen-to-square"></i></a>
+                        href="{{ route('dccomics.show', ['dccomic' => $curComic->id]) }}">
+                        <i class="fa-solid fa-plus"></i>
+                    </a>
+                    {{-- /btn add record --}}
 
-                    {{-- btn delete  --}}
-                    <form action="{{ route('dccomics.destroy', ['dccomic' => $curComic->id]) }}"
-                        method="POST" class="delete">
+                    {{-- btn modify record --}}
+                    <a class="btn btn-success text-decoration-none"
+                        href="{{ route('dccomics.edit', ['dccomic' => $curComic->id]) }}">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </a>
+                    {{-- /btn modify record --}}
+
+                    {{-- btn delete record --}}
+                    <form action="{{ route('dccomics.destroy', ['dccomic' => $curComic->id]) }}" method="POST"
+                        class="delete">
                         @csrf
                         @method('DELETE')
-                    <button type="button" data-comic-title="{{$curComic->title}}" class="btn btn-danger">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
+                        <button type="button" data-comic-title="{{ $curComic->title }}" class="btn btn-danger">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
                     </form>
+                    {{-- btn delete record --}}
+
                     <!-- modal -->
                     @include('partials.modal-delete')
                     {{-- /modal --}}
+
                 </div>
                 {{-- /section btn --}}
 
             </div>
+        @endforeach
+        {{-- /card-container  --}}
 
-            @endforeach
-        </div>
+    </div>
+    {{-- /container --}}
+
 @endsection
