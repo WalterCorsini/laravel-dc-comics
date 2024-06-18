f685c5f6d8c9cc43a62e3214d05dde9784bd7a53
WalterCorsini <corsini.walter@gmail.com>
2024-06-18 17:03:07 +0200

    add validate in storedccomicrequest

--- app/Http/Requests/StoreDcComicRequest.php
+++ app/Http/Requests/StoreDcComicRequest.php
@@ -29,4 +29,19 @@ class StoreDcComicRequest extends FormRequest
             'type'=> ['required','in:comic book,graphic novel']
         ];
     }
+
+    public function messages() {
+        return [
+            'title.required' => 'Il campo titolo è vuoto',
+            'title.min' => 'devi inserire un minimo di 10 caratteri',
+            'description.required' => 'devi inserire un minimo di 20 caratteri',
+            'price.required' => 'il campo prezzo non puo essere vuoto',
+            'price.min' => 'il campo prezzo non puo essere inferiore ad 1 euro',
+            'price.max' => 'il campo prezzo non puo essere inferiore ad 1000 euro',
+            'sale_date.required' => 'il campo data d\'uscita non puo essere vuoto',
+            'sale_date.before' => 'il campo data d\'uscita non puo essere una data futura',
+            'type.required' => 'il campo genere non puo essere vuoto',
+            'type.in' => 'il campo genere non è tra quelli indicati',
+        ];
+    }
 }
